4cece2dbf9b70305eed5ec1809c7bdf8516d168c
geniusnah <njhfilm@snu.ac.kr>
2023-05-30 23:21:07 +0900

    Update berryGame.js

--- berryGame.js
+++ berryGame.js
@@ -2,19 +2,109 @@ class berryGame extends Game {
   constructor(_idx, _gameList) {
     super(_idx, _gameList);
     this.gameName = "딸기당근수박참외메론";
+    this.turn = 0;
+    //버튼이미지로드 (1 딸기, 2 당근, 3 수박, 4 참외, 5 메론)
+    this.buttons = [1,2,3,4,5];
   }
-  round() {}
+  round() {
+    this.gameOver = false;
+    this.berryBGM();
+    while(!this.gameOver){
+      if(this.idx == 3){
+        this.playerTurn();
+      } else{
+        this.npcTurn();
+      }
+    }
+  }
+
+  berryBGM(){
+    text("딸기 당근 수박 참외 메론~게임!", w/2, h/2);
+    text("딸기 당근 수박 참외 메론~게임!", w/2, h/2);
+    //1.5초씩
+  }
+
+  playerTurn(){
+    displayButtons();
+    
+    this.turn++;
+
+  }
+
+  npcTurn(){
+    if(this.turn % 14 == 0){
+      text("딸기", w/3, h/3);
+
+    } else if(this.turn % 14 == 1 || this.turn % 14 == 13){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+
+    } else if(this.turn % 14 == 2 || this.turn % 14 == 12){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      
+    } else if(this.turn % 14 == 3 || this.turn % 14 == 11){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      text("참외", w/3, h/3);
+      
+    } else if(this.turn % 14 == 4 || this.turn % 14 == 10){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      text("참외", w/3, h/3);
+      text("메론", w/3, h/3);
+      
+    } else if(this.turn % 14 == 5 || this.turn % 14 == 9){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      text("참외", w/3, h/3);
+      text("메론", w/3, h/3);
+      text("딸기", w/3, h/3);
+      
+    } else if(this.turn % 14 == 6 || this.turn % 14 == 8){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      text("참외", w/3, h/3);
+      text("메론", w/3, h/3);
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      
+    } else if(this.turn % 14 == 7){
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      text("참외", w/3, h/3);
+      text("메론", w/3, h/3);
+      text("딸기", w/3, h/3);
+      text("당근", w/3, h/3);
+      text("수박", w/3, h/3);
+      
+    } 
+
+
+
+    this.turn++;
+  }
+
+  displayButtons(){
+    shuffleArray();
+    //buttons[] 순서대로 이미지 가져와서 배치 
+  }
+
+  shuffleArray() {
+    for (let i = 4; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.buttons[i], this.buttons[j]] = [this.buttons[j], this.buttons[i]]; // 배열의 두 원소를 랜덤하게 교환
+    }
+  }
 }
 
-
-
-class berryGame extends Game {
-  constructor(_idx, _player, _chars) {
-    super(_idx, _player, _chars);
-    this.idx = _idx;
-    this.player = _player;
-    this.chars = _chars;
-  }
+/*
   round() {
     turn = 0;
     while(gameIsOn){
@@ -47,10 +137,7 @@ class berryGame extends Game {
 
   }
   
-  berryBGM(){
-    Text("딸기 당근 수박 참외 메론~게임!");
-    Text("딸기 당근 수박 참외 메론~게임!");
-  }
+
 
   mousePressed(){
     if (mouseX mouseY~~~){
@@ -65,5 +152,4 @@ class berryGame extends Game {
       selectedButton = 5;
     }
   }
-}
-}
+}*/
