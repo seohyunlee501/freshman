0e12e670a59f77c4fd0bbdb27b0eb3253ab99fd0
seohyunlee501 <63445397+seohyunlee501@users.noreply.github.com>
2023-05-30 12:07:21 +0900

    alcholblood 반영

--- game.js
+++ game.js
@@ -33,14 +33,14 @@ class Game {
     textAlign(CENTER);
     textSize(20);
     text("혈중알콜농도", 0.9 * w, 0.25 * h);
+
     push();
     rectMode(CENTER);
     translate(w * 0.9, h * 0.6);
     rect(0, 0, w * 0.05, h * 0.6);
-
-    for (let i = 0; i < this.player.alcholblood; i++) {
-      fill(10 * i, 255 - 10 * i, 0);
-      rect(0, i / 10, 0.05 * w, 0.1 * h);
+    for (let i = 1; i <= this.player.alcholblood; i++) {
+      fill(10 * i, 255 - 10 * i, 20);
+      rect(0, 0.25 * h + (i - 1) / 10, 0.045 * w, 0.09 * h);
     }
     pop();
 
