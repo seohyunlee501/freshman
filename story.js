cfa807442d5e8971252031fb4db376a928cd1961
queramin <shinmj1024@snu.ac.kr>
2023-05-30 17:20:58 +0900

    undefined 오류

--- story.js
+++ story.js
@@ -7,11 +7,35 @@ class Story {
   display() {
     if (this.mode == 2) {
       // 도입 story
-      console.log("story 2");
-      mode = 3;
+      console.log("도입 story");
+      imageMode(CENTER);
+      console.log("이미지 모드 확인");
+      image(
+        bg2,
+        windowWidth * 0.5,
+        windowHeight * 0.5,
+        windowWidth,
+        windowHeight
+      );
+      console.log("이미지 로딩 확인");
+      // 플레이어 이미지
+      image(npc1Img[0], w * 0.8, h * 0.5, w * 0.3, h * 0.3);
+      fill(0);
+      rectMode(CENTER);
+      rect(w * 0.5, h * 0.1, w, h * 0.2);
+      rect(w * 0.5, h * 0.1, w, h * 0.2);
+      fill(255);
+      textMode(CENTER);
+      textSize(30);
+      text(
+        this.player.name +
+          "아 그거 알아? 오늘 수업 끝나고 신입생 환영회가 있대!",
+        w * 0.5,
+        h * 0.9
+      );
+      // mode = 3;
     } else if (this.mode == 5) {
       // 고학번 story
-      console.log("story 5");
     }
   }
 }
