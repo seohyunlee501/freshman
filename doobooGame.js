8573e76a91c8a5bb9c4b4979faa91f989c1d8481
geniusnah <njhfilm@snu.ac.kr>
2023-06-11 03:18:34 +0900

    .
    .

--- doobooGame.js
+++ doobooGame.js
@@ -6,20 +6,15 @@ class doobooGame extends Game {
     this.turn = 0;
     this.currentTime = millis();
     this.bgmOn = true;
+    this.dbCallOn = true;
+    this.dbIsSquareOn = true;
+    this.rhythmIsLifeOn = true;
+    this.idx = 0;
   }
-  round() {
-    if(this.turn == 0){
-      this.dbBgm();
-    } else {
-        if(this.this.idx == 3){
-          this.playerTurn();
-        } else {
-          this.npcTurn();
-        }
-    }
-  }
+  
 
   dbBgm(){
+    console.log('dbBgm');
     textSize(32);
     textAlign(CENTER);
     rectMode(CENTER);
@@ -58,7 +53,7 @@ class doobooGame extends Game {
     //6초 안에 key input이 없다면 rhythmIsLife() 호출 후 gameOver = true;하는 메소드를 구현하고 싶습니다...
     if(keyIsPressed){
       this.keyPressed();
-      this.dbCall(); //1.5초동안 텍스트 유지 후 사라지게 구현하고 싶습니다.
+      this.dbCall();
       if(this.dbCount != 1 && this.dbCount != 2 && this.dbCount != 4 && this.dbCount != 5){
         this.dbIsSquare();
         this.turn= 0;
@@ -75,6 +70,8 @@ class doobooGame extends Game {
   }
 
   npcTurn(){
+    console.log('npcturn');
+    this.currentTime = millis();
     if(this.turn > 10){
       this.dbCount = int(random(0,10));
       this.dbCall();
@@ -88,11 +85,11 @@ class doobooGame extends Game {
       }
     } else {
       let dbNum = [1,2,4,5];
-      this.dbCount = dbNum[int(random(0,5))];
-      
-        this.dbCall();
-        this.point();
-        this.turn++;
+      this.dbCount = dbNum[int(random(0,4))];
+      this.dbCallOn = true;
+      this.dbCall();
+      this.point();
+      this.turn++;
       }
     }
       
@@ -100,6 +97,7 @@ class doobooGame extends Game {
   
 
   point(){
+    console.log('point');
     if(this.dbCount == 1){
       this.idx -= 2;
       if(this.idx < 0){
@@ -126,53 +124,83 @@ class doobooGame extends Game {
   }
 
   dbCall(){
-    let x = (0.1)*w*(this.this.idx+1);
-
-
-    //1.5초동안 텍스트 유지 후 사라지게 구현하고 싶습니다.
-    if(this.dbCount == 1){
-      text("두부 한 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 2){
-      text("두부 두 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 4){
-      text("두부 네 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 5){
-      text("두부 다섯 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 3){
-      text("두부 세 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 0){
-      text("두부 빵 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 6){
-      text("두부 여섯 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 7){
-      text("두부 일곱 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 8){
-      text("두부 여덟 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 9){
-      text("두부 아홉 모!", x, 0.3*h);
-      
+    console.log('dbcall');
+    console.log(this.currentTime);
+    let x = (0.1)*w*(this.idx+1);
+    if(this.dbCallOn){
+      if(millis() - this.currentTime < 1200){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        if(this.dbCount == 1){
+          text("두부 한 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 2){
+          text("두부 두 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 4){
+          text("두부 네 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 5){
+          text("두부 다섯 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 3){
+          text("두부 세 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 0){
+          text("두부 빵 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 6){
+          text("두부 여섯 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 7){
+          text("두부 일곱 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 8){
+          text("두부 여덟 모!", x, 0.3*h);
+          
+        } else if(this.dbCount == 9){
+          text("두부 아홉 모!", x, 0.3*h);
+        }
+       } else {
+        this.dbCallOn = false;
+        this.turn++;
+        }
+      }
     }
-  }
+  
 
   dbIsSquare(){
-    //각 텍스트들이 한줄당 1.5초씩 나왔다 사라지게 하고 싶습니다.
-    text("두부는 네모!", w/2, h/2);
-    
-    text("두부는 네모!", w/2, h/2);
-    
-    text("네모! 네모!", w/2, h/2);
-    
-    text("네모! 네모! 네모!", w/2, h/2);
-    
+    console.log('dbissquare');
+    textSize(32);
+    textAlign(CENTER);
+    rectMode(CENTER);
+    if(this.dbIsSquareOn){
+      if(millis() - this.currentTime < 1200){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("두부는 네모!", w / 2, h / 2);
+      } else if(millis() - this.currentTime < 2400){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("두부는 네모!", w / 2, h / 2);
+      } else if(millis() - this.currentTime < 3600){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("네모! 네모!", w / 2, h / 2);
+      } else if(millis() - this.currentTime < 4800){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("네모! 네모! 네모!", w / 2, h / 2);
+      } else {
+        this.dbIsSquareOn = false;
+        this.turn++;
+      }
+    }
   }
 
 
@@ -181,6 +209,7 @@ class doobooGame extends Game {
   
 
   keyPressed(){
+    console.log(keyCode);
     if (keyCode === Digit1){
       this.dbCount = 1;
     } else if (keyCode === Digit2){
@@ -205,13 +234,48 @@ class doobooGame extends Game {
   }
 
   rhythmIsLife(){
-    text("박자는 생명!", w/2, h/2);
-    text("박자는 생명!", w/2, h/2);
-    text("생명! 생명! 생명!생명!생명!", w/2, h/2);
+    console.log('rhythmislife');
+    textSize(32);
+    textAlign(CENTER);
+    rectMode(CENTER);
+    if(this.rhythmIsLifeOn){
+      if(millis() - this.currentTime < 1200){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("박자는 생명!", w / 2, h / 2);
+      } else if(millis() - this.currentTime < 2400){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("박자는 생명!", w / 2, h / 2);
+      } else if(millis() - this.currentTime < 3600){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("생명! 생명!!", w / 2, h / 2);
+      } else if(millis() - this.currentTime < 4800){
+        fill(255);
+        rect(w / 2, h / 2, w / 3, h / 3);
+        fill(0);
+        text("생명! 생명! 생명!", w / 2, h / 2);
+      } else {
+        this.rhythmIsLifeOn = false;
+        this.turn++;
+      }
+    }
+  }
 
-    //각 텍스트들이 한줄당 1.5초씩 나왔다 사라지게 하고 싶습니다.
-
-    
+  round() {
+    if(this.turn == 0){
+      this.dbBgm();
+    } else {
+        if(this.idx == 3){
+          this.playerTurn();
+        } else {
+          this.npcTurn();
+        }
+    }
   }
 
 }
