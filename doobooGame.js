b4f7ffe94f5326c6c696c62f62f2f213286a438d
geniusnah <njhfilm@snu.ac.kr>
2023-06-11 05:56:13 +0900

    두부 1차 완성...

--- doobooGame.js
+++ doobooGame.js
@@ -14,30 +14,30 @@ class doobooGame extends Game {
     this.endTime = 0;
     this.loseIssue = 0;
   }
-  
 
-  dbBgm(){
+
+  dbBgm() {
     console.log('dbBgm');
     textSize(32);
     textAlign(CENTER);
     rectMode(CENTER);
-    if(this.bgmOn){
-      if(millis() - this.startTime < 1200){
+    if (this.bgmOn) {
+      if (millis() - this.startTime < 1200) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
         text("두~부 두부 두부", w / 2, h / 2);
-      } else if(millis() - this.startTime < 2400){
+      } else if (millis() - this.startTime < 2400) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
         text("으쌰!으쌰!으쌰!으쌰!", w / 2, h / 2);
-      } else if(millis() - this.startTime < 3600){
+      } else if (millis() - this.startTime < 3600) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
         text("두~부 두부 두부", w / 2, h / 2);
-      } else if(millis() - this.startTime < 4800){
+      } else if (millis() - this.startTime < 4800) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
@@ -47,12 +47,12 @@ class doobooGame extends Game {
         this.turn++;
       }
     }
-    
+
   }
 
 
 
-  playerTurn(){
+  playerTurn() {
     if (!this.userPlayed) {
       this.infoStarted = true;
       this.infoTime = millis();
@@ -64,7 +64,7 @@ class doobooGame extends Game {
         textAlign(CENTER);
         rectMode(CENTER);
         fill(0);
-        text("두부 모수를 키보드에 입력해 주세요.", w / 2, 2*h / 3);
+        text("두부 모수를 키보드에 입력해 주세요.", w / 2, h / 4);
       } else {
         this.infoStarted = false;
       }
@@ -78,41 +78,10 @@ class doobooGame extends Game {
       }
     } else if (this.turnStarted) {
       if (millis() - this.currentTime < 1200) {
-      this.dbCall();
+        this.dbCall();
       } else {
-        if(this.dbCount != 1 && this.dbCount != 2 && this.dbCount != 4 && this.dbCount != 5){
-        this.loseIssue = 1;
-        this.gameend();
-      } else {
-        this.point();
-        this.turnStarted = false;
-        this.turn++;
-      }
-    }
-  }
-}
-
-
-  npcTurn(){
-    console.log('npcturn');
-    if (this.everyone[this.idx].die) {
-      this.idx++;
-      this.idx = this.idx % 6;
-    } else {
-      if (!this.turnStarted) {
-        if(this.turn > 10){
-          this.dbCount = int(random(0,10));
-        } else {
-          let dbNum = [1,2,4,5];
-          this.dbCount = dbNum[int(random(0,4))];
-        }
-        this.turnStarted = true;
-        this.currentTime = millis();
-      } else if (this.turnStarted) {
-        if (millis() - this.currentTime < 1600) {
-          this.dbCall();
-        } else {
-          if(this.dbCount != 1 && this.dbCount != 2 && this.dbCount != 4 && this.dbCount != 5){
+        if (this.dbCount != 1 && this.dbCount != 2 && this.dbCount != 4 && this.dbCount != 5) {
+          this.loseIssue = 1;
           this.gameend();
         } else {
           this.point();
@@ -122,125 +91,155 @@ class doobooGame extends Game {
       }
     }
   }
-}
 
-  point(){
+
+  npcTurn() {
+    if (this.everyone[this.idx].die) {
+      this.idx++;
+      this.idx = this.idx % 6;
+    } else {
+      if (!this.turnStarted) {
+        if (this.turn > 10) {
+          this.dbCount = int(random(0, 10));
+        } else {
+          let dbNum = [1, 2, 4, 5];
+          this.dbCount = dbNum[int(random(0, 4))];
+        }
+        this.turnStarted = true;
+        this.currentTime = millis();
+      } else if (this.turnStarted) {
+        if (millis() - this.currentTime < 1600) {
+          this.dbCall();
+        } else {
+          if (this.dbCount != 1 && this.dbCount != 2 && this.dbCount != 4 && this.dbCount != 5) {
+            this.loseIssue = 1;
+            this.gameend();
+          } else {
+            this.point();
+            this.turnStarted = false;
+            this.turn++;
+          }
+        }
+      }
+    }
+  }
+
+  point() {
     console.log('point');
-    if(this.dbCount == 1){
+    if (this.dbCount == 1) {
       this.idx -= 2;
-      if(this.idx < 0){
+      if (this.idx < 0) {
         this.idx += 6;
       }
-    } else if(this.dbCount == 2){
+    } else if (this.dbCount == 2) {
       this.idx -= 1;
-      if(this.idx < 0){
+      if (this.idx < 0) {
         this.idx += 6;
       }
-      }
-     else if(this.dbCount == 4){
+    }
+    else if (this.dbCount == 4) {
       this.idx += 1;
-      if(this.idx > 5){
+      if (this.idx > 5) {
         this.idx -= 6;
       }
-    } else if(this.dbCount == 5){
+    } else if (this.dbCount == 5) {
       this.idx += 2;
-      if(this.idx > 5){
+      if (this.idx > 5) {
         this.idx -= 6;
       }
     }
 
   }
 
-  dbCall(){
+  dbCall() {
     console.log('dbcall');
     fill(0);
     let x = 0.2 * w + 0.17 * h * this.idx;
-    if(this.dbCount == 1){
-      text("두부 한 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 2){
-      text("두부 두 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 4){
-      text("두부 네 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 5){
-      text("두부 다섯 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 3){
-      text("두부 세 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 0){
-      text("두부 빵 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 6){
-      text("두부 여섯 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 7){
-      text("두부 일곱 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 8){
-      text("두부 여덟 모!", x, 0.3*h);
-      
-    } else if(this.dbCount == 9){
-      text("두부 아홉 모!", x, 0.3*h);
+    if (this.dbCount == 1) {
+      text("두부 한 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 2) {
+      text("두부 두 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 4) {
+      text("두부 네 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 5) {
+      text("두부 다섯 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 3) {
+      text("두부 세 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 0) {
+      text("두부 빵 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 6) {
+      text("두부 여섯 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 7) {
+      text("두부 일곱 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 8) {
+      text("두부 여덟 모!", x, 0.3 * h);
+
+    } else if (this.dbCount == 9) {
+      text("두부 아홉 모!", x, 0.3 * h);
     }
   }
 
-  
 
-  dbIsSquare(){
+
+  dbIsSquare() {
     console.log('dbissquare');
     textSize(32);
     textAlign(CENTER);
     rectMode(CENTER);
-    if(this.dbIsSquareOn){
-      if(millis() - this.endTime < 1200){
-        fill(255);
-        rect(w / 2, h / 2, w / 3, h / 3);
-        fill(0);
-        text("두부는 네모!", w / 2, h / 2);
-      } else if(millis() - this.endTime < 2400){
-        fill(255);
-        rect(w / 2, h / 2, w / 3, h / 3);
-        fill(0);
-        text("두부는 네모!", w / 2, h / 2);
-      } else if(millis() - this.endTime < 3600){
-        fill(255);
-        rect(w / 2, h / 2, w / 3, h / 3);
-        fill(0);
-        text("네모! 네모!", w / 2, h / 2);
-      } else if(millis() - this.endTime < 4800){
-        fill(255);
-        rect(w / 2, h / 2, w / 3, h / 3);
-        fill(0);
-        text("네모! 네모! 네모!", w / 2, h / 2);
-      }
+    if (millis() - this.endTime < 1200) {
+      fill(255);
+      rect(w / 2, h / 2, w / 3, h / 3);
+      fill(0);
+      text("두부는 네모!", w / 2, h / 2);
+    } else if (millis() - this.endTime < 2400) {
+      fill(255);
+      rect(w / 2, h / 2, w / 3, h / 3);
+      fill(0);
+      text("두부는 네모!", w / 2, h / 2);
+    } else if (millis() - this.endTime < 3600) {
+      fill(255);
+      rect(w / 2, h / 2, w / 3, h / 3);
+      fill(0);
+      text("네모! 네모!", w / 2, h / 2);
+    } else if (millis() - this.endTime < 4800) {
+      fill(255);
+      rect(w / 2, h / 2, w / 3, h / 3);
+      fill(0);
+      text("네모! 네모! 네모!", w / 2, h / 2);
     }
+
   }
 
-  rhythmIsLife(){
+  rhythmIsLife() {
     console.log('rhythmislife');
     textSize(32);
     textAlign(CENTER);
     rectMode(CENTER);
-    if(this.rhythmIsLifeOn){
-      if(millis() - this.endTime < 1200){
+    if (this.rhythmIsLifeOn) {
+      if (millis() - this.endTime < 1200) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
         text("박자는 생명!", w / 2, h / 2);
-      } else if(millis() - this.endTime < 2400){
+      } else if (millis() - this.endTime < 2400) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
         text("박자는 생명!", w / 2, h / 2);
-      } else if(millis() - this.endTime < 3600){
+      } else if (millis() - this.endTime < 3600) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
         text("생명! 생명!!", w / 2, h / 2);
-      } else if(millis() - this.endTime < 4800){
+      } else if (millis() - this.endTime < 4800) {
         fill(255);
         rect(w / 2, h / 2, w / 3, h / 3);
         fill(0);
@@ -255,9 +254,9 @@ class doobooGame extends Game {
       this.endTime = millis();
     } else {
       if (millis() - this.endTime < 4800) {
-        if(this.loseIssue == 1){
+        if (this.loseIssue == 1) {
           this.dbIsSquare();
-        } else if(this.loseIssue == 2) {
+        } else if (this.loseIssue == 2) {
           this.rhythmIsLife();
         }
       } else {
@@ -269,16 +268,17 @@ class doobooGame extends Game {
   }
 
   round() {
-    if(this.turn == 0){
+    if (this.turn == 0) {
       this.dbBgm();
     } else {
-        if(this.idx == 3){
-          this.playerTurn();
-        } else {
-          this.npcTurn();
-        }
+      if (this.idx == 3) {
+        this.playerTurn();
+      } else {
+        this.npcTurn();
+      }
     }
   }
 
 }
+
 
